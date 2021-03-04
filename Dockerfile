c1da6ec6ff6da970e49d99e40eaf423ca09e1b05
GeekXwb <geekxwb@gmail.com>
2021-03-04 21:12:40 +0800

    更新

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ COPY sources.list /etc/apt/
 RUN cd /root && dpkg -i chromev88.deb ; exit 0
 RUN apt update
 RUN DEBIAN_FRONTEND=noninteractive apt --fix-broken install -y
-RUN apt install python3 python3-pip -y && pip3 install selenium requests
+RUN apt install python3 python3-pip -y && pip3 install selenium requests retrying pycryptodomex
 RUN cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 RUN dpkg-reconfigure -f noninteractive tzdata
 CMD python3 /root/run.py
---
+++ notification.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
+'''
+this module can notify u the report starus by different ways
+if you are not developer,you must NOT write anything in this file
+'''
+def mail(username,yesorno,historyurl,my_user,my_sender,SMTPdomain,SMTPauth,datetime,reporttype):
+    ret = True
+    if reporttype == "home":
+        type = "居家"
+    elif reporttype == "morn":
+        type = "晨检"
+    elif reporttype == "dorm":
+        type = "归寝"
+    try:
+        msg = MIMEText(username+':' + type + '打卡' ':'+ yesorno + '！'+"\n"+"访问以下链接可查看打卡历史记录：\n"+historyurl , 'plain', 'utf-8')
+        msg['From'] = formataddr(["打卡提醒", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+        msg['To'] = formataddr(["您好，订阅者", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+        msg['Subject'] = datetime+type+"打卡"+yesorno  # 主题
+        server = smtplib.SMTP_SSL(SMTPdomain, 465)  # 使用SSL发送
+        server.login(my_sender, SMTPauth)  # SMTP密码，这里是我的的密码
+        server.sendmail(my_sender, [my_user, ], msg.as_string())
+        server.quit()
+    except:  # 如果try中的语句没有执行，则会执行下面的ret=False
+        ret = False
+    return ret
